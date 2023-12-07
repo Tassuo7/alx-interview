2baf1b581a590941488496bd554b12048becd281
Tassuo7 <abdelhamid.jack@gmail.com>
2023-12-06 16:08:07 -0800

    fix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,17 +7,17 @@ and each box may contain keys to the other boxes.
 
 
 def canUnlockAll(boxes):
-	"""
-	method that determines if all the boxes can be opened
-	"""
-	n = len(boxes)
-	open_boxes = set([0])
-	close_boxes = set(boxes[0]).difference(set([0]))
-	while len(close_boxes) > 0:
-		nbBox = close_boxes.pop()
-		if not nbBox or nbBox >= n or nbBox < 0:
-			continue
-		if nbBox not in open_boxes:
-			close_boxes = close_boxes.union(boxes[nbBox])
-			open_boxes.add(nbBox)
-	return n == len(open_boxes)
+    """
+    method that determines if all the boxes can be opened
+    """
+    n = len(boxes)
+    open_boxes = set([0])
+    close_boxes = set(boxes[0]).difference(set([0]))
+    while len(close_boxes) > 0:
+        nbBox = close_boxes.pop()
+        if not nbBox or nbBox >= n or nbBox < 0:
+            continue
+        if nbBox not in open_boxes:
+            close_boxes = close_boxes.union(boxes[nbBox])
+            open_boxes.add(nbBox)
+    return n == len(open_boxes)
