3f3c9c3681676aa3c314b9eb18090a0eb43112d2
Tassuo7 <abdelhamid.jack@gmail.com>
2024-01-02 15:15:35 -0800

    fix output

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -35,11 +35,3 @@ finally:
     for key, value in sorted(cache.items()):
         if value != 0:
             print('{}: {}'.format(key, value))
-except Exception as err:
-    pass
-
-finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
