f10a8dd71cce0d33dbf782d80afffb912d4768d5
Tassuo7 <abdelhamid.jack@gmail.com>
2024-01-02 15:09:09 -0800

    0-validate_utf8.py

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    Check if a set of integers are valid utf-8 format
+    data = [1,2,3 ..., n]
+    return True if all integers in list are valid utf-8 format
+    Otherwise False
+"""
+
+
+def validUTF8(data):
+    """Check if a set of integers are valid utf-8 format"""
+    num_bytes = 0
+    for char_d in data:
+        byte = char_d & 0xff
+        if num_bytes:
+            if (byte >> 6 == 1 or byte >> 6 == 3):
+                return False
+            num_bytes -= 1
+            continue
+
+        while 7 - num_bytes and byte & (1 << (7 - num_bytes)):
+            num_bytes += 1
+        if num_bytes == 1 or num_bytes > 4:
+            return False
+        num_bytes = max(num_bytes - 1, 0)
+
+    if num_bytes:
+        return False
+    return True
